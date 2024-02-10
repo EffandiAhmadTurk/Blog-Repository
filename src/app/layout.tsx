71c0aa506f93db37c5cd2b3e072f12468fa99b61
effandiahmed <effandiahmed32@gmail.com>
2024-02-10 23:49:41 +0500

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      /* eslint-disable */
       <body className={inter.className}>{children}</body>
+      /* eslint-enable */
     </html>
   );
 }
